36a7f471bbeb9d99ea853d0b8e0aa82d69ae8c4f
Vinita Peter <vinita.peter@gadgeon.com>
2022-08-01 15:16:28 +0530

    url changed to cloud url

--- src/app/submit-service.service.ts
+++ src/app/submit-service.service.ts
@@ -13,8 +13,8 @@ export class SubmitServiceService {
  
     console.log(username);
     console.log(password);
-    //const postLoginUrl = 'https://20.24.139.62/' + 'login?username=' + username + '&&password=' + password;
-    const postLoginUrl = 'http://localhost:8080/' + 'login?username=' + username + '&&password=' + password;
+    const postLoginUrl = 'https://20.24.139.62/' + 'login?username=' + username + '&&password=' + password;
+    //const postLoginUrl = 'http://localhost:8080/' + 'login?username=' + username + '&&password=' + password;
     const res =  this.http.post(postLoginUrl, {});
     
     console.log(res)
